707fcc30744792f2caa34ad95608d5c73e3e82aa
Filipe Fenrich <foolemdev@gmail.com>
2017-10-11 21:04:51 -0300

    animation on scroll

--- assets/js/application.js
+++ assets/js/application.js
@@ -1,3 +1,19 @@
-$('.carousel').carousel({
-  interval: 5000 //3500
-});
+$(document).ready(function(){
+
+  $(window).scroll(function() {
+    var div = $(this);
+    if(div.scrollTop() !== 0) {
+
+      
+      $("#nav").css("margin-top", -30);
+
+      //$("#nav").addClass('fixed-top');
+    }
+    else {
+      //$("#nav").removeClass('fixed-top');
+      //$("#top-nav").addClass('fixed-top');
+      $("#top-bar").css("opacity", 1);
+      $("#nav").css("margin-top", 0);
+    }
+  });
+})
