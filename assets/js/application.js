5e21284bd3eeec7c2a4636cd00925311122c09c1
Filipe Fenrich <foolemdev@gmail.com>
2017-10-13 21:26:12 -0300

    aaa

--- assets/js/application.js
+++ assets/js/application.js
@@ -1,19 +1,17 @@
-$(document).ready(function(){
 
+$(document).ready(function(){
+  setTimeout(function(){
+    $('#loader').addClass('animated fadeOut d-none');
+  }, 3500);
   $(window).scroll(function() {
     var div = $(this);
     if(div.scrollTop() !== 0) {
-
-      
       $("#nav").css("margin-top", -30);
-
-      //$("#nav").addClass('fixed-top');
     }
     else {
-      //$("#nav").removeClass('fixed-top');
-      //$("#top-nav").addClass('fixed-top');
       $("#top-bar").css("opacity", 1);
       $("#nav").css("margin-top", 0);
     }
   });
-})
+
+});
