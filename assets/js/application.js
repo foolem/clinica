3ada1ad26d59ab7d917af08b769d522e6d6f0007
Filipe Fenrich <foolemdev@gmail.com>
2017-10-20 13:40:11 -0200

    return to top

--- assets/js/application.js
+++ assets/js/application.js
@@ -1,4 +1,21 @@
 $(document).ready(function () {
+
+  $(window).scroll(function() {
+    if ($(this).scrollTop()) {
+        $('#toTop').fadeIn();
+    } else {
+        $('#toTop').fadeOut();
+    }
+});
+
+$("#toTop").click(function () {
+   //1 second of animation time
+   //html works for FFX but not Chrome
+   //body works for Chrome but not FFX
+   //This strange selector seems to work universally
+   $("html, body").animate({scrollTop: 0}, 1000);
+});
+
     //rotation speed and timer
     var speed = 9000;
 
