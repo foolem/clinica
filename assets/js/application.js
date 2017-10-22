81fc0672cab6be75a524bd6c373abf6d420bc7c2
Filipe Fenrich <foolemdev@gmail.com>
2017-10-21 23:37:41 -0200

    added jquery anchors

--- assets/js/application.js
+++ assets/js/application.js
@@ -1,5 +1,59 @@
 $(document).ready(function () {
 
+  $('#initial-anchor').click(function(){
+    $('html, body').animate({
+        scrollTop: $( $(this).attr('href') ).offset().top - 40
+    }, 500);
+    return false;
+  });
+  $('#where-anchor').click(function(){
+    $('html, body').animate({
+        scrollTop: $( $(this).attr('href') ).offset().top - 70
+    }, 500);
+    return false;
+  });
+  $('#about-anchor').click(function(){
+    $('html, body').animate({
+        scrollTop: $( $(this).attr('href') ).offset().top - 60
+    }, 500);
+    return false;
+  });
+  $('#contact-anchor').click(function(){
+    $('html, body').animate({
+        scrollTop: $( $(this).attr('href') ).offset().top - 60
+    }, 500);
+    return false;
+  });
+
+  $('#do-flip').mouseover(function(){
+    $('#flip').addClass('hvr-flips');
+    $('#flip').removeClass('hvr-flips-back');
+  }).mouseleave(function(){
+    $('#flip').addClass('hvr-flips-back');
+    $('#flip').removeClass('hvr-flips');
+  });
+  $('#do-flip2').mouseover(function(){
+    $('#flip2').addClass('hvr-flips');
+    $('#flip2').removeClass('hvr-flips-back');
+  }).mouseleave(function(){
+    $('#flip2').addClass('hvr-flips-back');
+    $('#flip2').removeClass('hvr-flips');
+  });
+  $('#do-flip3').mouseover(function(){
+    $('#flip3').addClass('hvr-flips');
+    $('#flip3').removeClass('hvr-flips-back');
+  }).mouseleave(function(){
+    $('#flip3').addClass('hvr-flips-back');
+    $('#flip3').removeClass('hvr-flips');
+  });
+  $('#do-flip4').mouseover(function(){
+    $('#flip4').addClass('hvr-flips');
+    $('#flip4').removeClass('hvr-flips-back');
+  }).mouseleave(function(){
+    $('#flip4').addClass('hvr-flips-back');
+    $('#flip4').removeClass('hvr-flips');
+  });
+
   $(window).scroll(function() {
     if ($(this).scrollTop()) {
         $('#toTop').fadeIn();
@@ -16,76 +70,7 @@ $("#toTop").click(function () {
    $("html, body").animate({scrollTop: 0}, 1000);
 });
 
-    //rotation speed and timer
-    var speed = 9000;
 
-    var run = setInterval(rotate, speed);
-    var slides = $('.slide');
-    var container = $('#slides ul');
-    var elm = container.find(':first-child').prop("tagName");
-    var item_width = container.width();
-    var previous = 'prev'; //id of previous button
-    var next = 'next'; //id of next button
-    slides.width(item_width); //set the slides to the correct pixel width
-    container.parent().width(item_width);
-    container.width(slides.length * item_width); //set the slides container to the correct total width
-    container.find(elm + ':first').before(container.find(elm + ':last'));
-    resetSlides();
-
-
-    //if user clicked on prev button
-
-    $('#buttons a').click(function (e) {
-        //slide the item
-
-        if (container.is(':animated')) {
-            return false;
-        }
-        if (e.target.id == previous) {
-            container.stop().animate({
-                'left': 0
-            }, 1500, function () {
-                container.find(elm + ':first').before(container.find(elm + ':last'));
-                resetSlides();
-            });
-        }
-
-        if (e.target.id == next) {
-            container.stop().animate({
-                'left': item_width * -2
-            }, 1500, function () {
-                container.find(elm + ':last').after(container.find(elm + ':first'));
-                resetSlides();
-            });
-        }
-
-        //cancel the link behavior
-        return false;
-
-    });
-
-    //if mouse hover, pause the auto rotation, otherwise rotate it
-    container.parent().mouseenter(function () {
-        clearInterval(run);
-    }).mouseleave(function () {
-        run = setInterval(rotate, speed);
-    });
-
-
-    function resetSlides() {
-        //and adjust the container so current is in the frame
-        container.css({
-            'left': -1 * item_width
-        });
-    }
-
-
-//a simple function to click next link
-//a timer will call this function, and the rotation will begin
-
-function rotate() {
-    $('#next').click();
-}
 
 
   $(function () {
